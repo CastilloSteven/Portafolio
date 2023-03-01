aa99432b763d25b169c00bec597b2ec51531aa03
Steven <sacastillo889@misena.edu.co>
2023-03-01 17:17:52 -0500

    nuevo proyecto

--- js/proyectos.js
+++ js/proyectos.js
@@ -4,6 +4,11 @@ const misProyectos = [
         nombre  : 'Galeria de Fate / Stay Night',
         imagen  : 'assets/proyectoFate.png',
         urlGithub  : 'https://github.com/CastilloSteven/Fate-Web',
-        urlWeb     : '',
+    },
+    {
+        Id  : 2,
+        nombre  : 'Easybank landing page',
+        imagen  : 'assets/proyectoLandingPage.png',
+        urlGithub  : 'https://github.com/CastilloSteven/Easybank-landing-page',
     },
 ]
