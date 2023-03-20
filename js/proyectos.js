0af9a4e0362720e6b56906c77f6e1e91404b6d37
Steven <sacastillo889@misena.edu.co>
2023-03-20 14:09:33 -0500

    nuevos estilos y lenguajes

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,14 +1,8 @@
 const misProyectos = [
     {
-        Id  : 1,
-        nombre  : 'Galeria de Fate / Stay Night',
-        imagen  : 'assets/proyectoFate.png',
-        urlGithub  : 'https://github.com/CastilloSteven/Fate-Web',
-    },
-    {
-        Id  : 2,
         nombre  : 'Easybank landing page',
         imagen  : 'assets/proyectoLandingPage.png',
         urlGithub  : 'https://github.com/CastilloSteven/Easybank-landing-page',
+        lenguajes : ['HTML','CSS','JavaScript']
     },
 ]
